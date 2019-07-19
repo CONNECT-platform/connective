377150837cfb98d2f519f4cf05583f0c04d9c682
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2019-07-19 15:01:24 +0200

    add line copy to docs

--- docs/assets/main.js
+++ docs/assets/main.js
@@ -21,7 +21,22 @@ window.addEventListener('load', function() {
   });
 
   document.querySelectorAll('.overlay').forEach(function(node) {
-    console.log(node);
     node.addEventListener('click', function(){hideOverlay(node)});
+  });
+
+  document.querySelectorAll('code.typescript').forEach(function(node) {
+    let lines = node.innerHTML.split('\n');
+    node.innerHTML = '';
+    for (var i = 0; i < lines.length; i++)
+      if (lines[i].trim().length > 0)
+        node.innerHTML += '<div class="line">' + lines[i] + '</div>';
+      else node.innerHTML += '<br>';
+  });
+
+  new ClipboardJS('code .line', {
+      text: function(trigger) {
+        showOverlay(copyConfirm, 2000);
+        return trigger.textContent;
+      }
   });
 });
