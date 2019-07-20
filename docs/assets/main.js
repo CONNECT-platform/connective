e4db6e6e8e4b8d6f3068f92dbe057ea0dc530e3c
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2019-07-21 00:35:14 +0200

    add toc to website, add more content

--- docs/assets/main.js
+++ docs/assets/main.js
@@ -13,6 +13,9 @@ function hideOverlay(overlay) {
 window.addEventListener('load', function() {
   var copyConfirm = document.getElementById('copy-confirm');
 
+  //
+  // copy whole code (like install instructions)
+  //
   new ClipboardJS('pre[copy] code', {
       text: function(trigger) {
         showOverlay(copyConfirm, 2000);
@@ -24,6 +27,11 @@ window.addEventListener('load', function() {
     node.addEventListener('click', function(){hideOverlay(node)});
   });
 
+
+  //
+  // break javascript code into distinct lines
+  // make each line copiable
+  //
   document.querySelectorAll('code.javascript').forEach(function(node) {
     let lines = node.innerHTML.split('\n');
     node.innerHTML = '';
@@ -40,10 +48,43 @@ window.addEventListener('load', function() {
       }
   });
 
+  //
+  // make the links of titles copiable
+  //
   new ClipboardJS('h1[id], h2[id], h3[id]', {
     text: function(trigger) {
       showOverlay(copyConfirm, 2000);
       return location.protocol+'//'+location.host+location.pathname+'#'+trigger.getAttribute('id');
     }
-  })
+  });
+
+  //
+  // make the toc
+  //
+  var div = document.createElement('div');
+  var titles = document.querySelectorAll('h1, h2, h3');
+  var tocels = {};
+  titles.forEach(function(node) {
+    var a = document.createElement('a');
+    a.setAttribute('href', location.protocol+'//'+location.host+location.pathname+'#'+node.getAttribute('id'));
+    a.textContent = node.textContent;
+    a.classList.add(node.tagName.toLowerCase());
+    tocels[node.getAttribute('id')] = a;
+    div.appendChild(a);
+    div.appendChild(document.createElement('br'));
+  });
+  document.getElementById('toc').appendChild(div);
+  window.addEventListener('scroll', function() {
+    var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+    var current = undefined;
+    titles.forEach(function(node) { if (node.offsetTop <= scrollTop + window.innerHeight) current = node; });
+    titles.forEach(function(node) {
+      var tocel = tocels[node.getAttribute('id')];
+      if ((node.offsetTop >= scrollTop &&
+        node.offsetTop <= scrollTop + window.innerHeight) || node == current)
+        tocel.classList.add('visible');
+      else
+        tocel.classList.remove('visible');
+    });
+  });
 });
